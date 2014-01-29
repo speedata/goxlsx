3100d986727815c38b49a476985fa3aad1164a74
Patrick Gundlach <gundlach@speedata.de>
2014-01-29 09:50:02 +0100

    Drop goconvey

--- xlsx_test.go
+++ xlsx_test.go
@@ -1,33 +1,39 @@
 package goxlsx
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
 	"path/filepath"
 	"testing"
 )
 
 func TestOpenFile(t *testing.T) {
-	Convey("Trying to open sample worksheet", t, func() {
-		xlsx, err := OpenFile(filepath.Join("_testdata", "Worksheet1.xlsx"))
-		if err != nil {
-			t.Error(err)
-		}
-		So(len(xlsx.Worksheets), ShouldEqual, 2)
+	xlsx, err := OpenFile(filepath.Join("_testdata", "Worksheet1.xlsx"))
+	if err != nil {
+		t.Error(err)
+	}
+	if len(xlsx.Worksheets) != 2 {
+		t.Error("num of worksheets != 2")
+	}
 
-		Convey("Looking at the first worksheet", func() {
-			ws, err := xlsx.GetWorksheet(0)
-			So(err, ShouldBeNil)
-			So(ws.filename, ShouldEqual, "xl/worksheets/sheet1.xml")
-			So(len(ws.rows), ShouldEqual, 5)
-			Convey("Looking at first row", func() {
-				row := ws.rows[1]
-				So(row.Cells[1].Value, ShouldEqual, "A")
-				So(row.Cells[2].Value, ShouldEqual, "B")
-			})
+	ws, err := xlsx.GetWorksheet(0)
+	if err != nil {
+		t.Error(err)
+	}
+	if ws.filename != "xl/worksheets/sheet1.xml" {
+		t.Error("filename mismatch")
+	}
+	if len(ws.rows) != 5 {
+		t.Error("ws.rows != 5")
+	}
 
-			Convey("Getting Cell (1,1)", func() {
-				So(ws.Cell(1, 1), ShouldEqual, "A")
-			})
-		})
-	})
+	row := ws.rows[1]
+	if row.Cells[1].Value != "A" {
+		t.Error("First value shoudl be A")
+	}
+	if row.Cells[2].Value != "B" {
+		t.Error("Second value shoudl be B")
+	}
+	if ws.Cell(1, 1) != "A" {
+		t.Error("1,1 should be A")
+	}
+
 }
